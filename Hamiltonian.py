5ac3d170b96aea61335b7f05fc1e4ab7a87e1d31
Gil Tabak <tabak.gil@gmail.com>
2016-04-02 16:12:33 -0700

    added files

---
+++ Hamiltonian.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 31 2015
+
+@author: gil
+@title: Hamiltonian.py
+"""
+
+import Roots
+import Potapov
+import Examples
+import functions
+
+import numpy as np
+import numpy.linalg as la
+import sympy as sp
+import itertools
+
+class Hamiltonian():
+    def __init__(self,roots,modes,delays,delay_indices,start_nonlin,
+        duration_nonlin,indices_of_refraction,chi_order,photons_annihilated):
+        self.roots = roots
+        self.modes = modes
+        self.delays = delays
+        self.delay_indices = delay_indices
+        self.start_nonlin = start_nonlin
+        self.duration_nonlin = duration_nonlin
+        self.indices_of_refraction = indices_of_refraction
+        self.chi_order=chi_order
+        self.photons_annihilated=photons_annihilated
+        self.m = len(roots)
+        self.a = [sp.symbols('a_'+str(i)) for i in range(self.m)]
+        self.a_H = [sp.symbols('a^H_'+str(i)) for i in range(self.m)]
+        self.H = None
+
+    def make_nonlin_term_sp(self,combination,pm_arr):
+        '''
+        Make symbolic nonlinear term using sympy
+
+        Args:
+            combination (tuple/list of integers): which terms to include
+            pm_arr (tuple/list of +1 and -1): creation and annihilation
+                indicators for the respective terms in combination.
+        Returns:
+            symbolic expression for the combination of creation and annihilation
+            operators.
+        '''
+        r = 1.
+        for index,sign in zip(combination,pm_arr):
+            if sign == 1:
+                r*= self.a_H[index]
+            else:
+                r *= self.a[index]
+        return r
+
+    def make_list_of_pm_arr(self,):
+        '''
+        Returns:
+            List of lists of +1 and -1. The length of each list is chi_order+1.
+            The number of -1's in each list is the number of photons annihilated.
+            Each unique combination exists once.
+        '''
+        plus_minus_combinations = list(itertools.combinations(
+            range(self.chi_order + 1), self.photons_annihilated))  ## pick which fields are annihilated
+        list_of_pm_arr = []
+        for tup in plus_minus_combinations:
+            ls = [1]*(self.chi_order+1)
+            for i in tup:
+                ls[i]=-1
+            list_of_pm_arr.append(ls)
+        return list_of_pm_arr
+
+    def weight(self,combination,pm_arr):
+        '''
+        The weight to give to each nonlinear term characterized by the given
+        combination and pm_arr.
+
+        Args:
+            combination (list/tuple of integers): which modes/roots to pick
+            pm_arr (list of +1 and -1): creation and annihilation of modes
+        Returns:
+            The weight to add to the Hamiltonian
+        '''
+        roots_to_use = np.array([self.roots[i] for i in combination])
+        modes_to_use = [self.modes[i] for i in combination]
+        return functions.make_nonlinear_interaction(
+                    roots_to_use, modes_to_use, self.delays, self.delay_indices,
+                    self.start_nonlin, self.duration_nonlin, pm_arr,
+                    self.indices_of_refraction)
+
+    def make_weights(self,):
+        '''
+        Make a dict to store the weights for the selected components and the
+        creation/annihilation information.
+
+        Returns:
+            A dictionary of weights. Each key is a tuple of a combination tuple
+            and a pm_arr tuple (a tuple of +1 and -1).
+        '''
+        ## TODO: add a priori check to restrict exponential growth on the number
+        ## of nonlienar coefficients
+        list_of_pm_arr = self.make_list_of_pm_arr()
+        weights = {}
+        for pm_arr in list_of_pm_arr:
+            field_combinations = itertools.combinations_with_replacement(
+                range(self.m), self.chi_order+1)
+            for combination in field_combinations:
+                weights[tuple(combination),tuple(pm_arr)] = self.weight(combination,pm_arr)
+        return weights
+
+    def make_nonlin_H(self,eps=1e-5):
+        '''
+        Make a nonlinear Hamiltonian based on nonlinear interaction terms
+        Args:
+            eps (optional[float]): Cutoff for the significance of a particular term.
+        Returns:
+            A symbolic expression for the nonlinear Hamiltonian.
+        '''
+
+        weights = self.make_weights()
+        significant_weights =  {k:v for k,v in weights.iteritems() if abs(v) > eps}
+
+        H_nonlin_sp = 0.
+        for combination,pm_arr in significant_weights:
+            H_nonlin_sp += (self.make_nonlin_term_sp(combination,pm_arr) *
+                            significant_weights[combination,pm_arr])
+        return H_nonlin_sp
+
+    def make_lin_H(self,Omega):
+        '''
+        Make a linear Hamiltonian based on Omega.
+        Args:
+            Omega (complex-valued matrix) describes the Hamiltonian of the system.
+        Returns:
+            A symbolic expression for the nonlinear Hamiltonian.
+        '''
+        H_lin_sp = 0.
+        for i in range(self.m):
+            for j in range(self.m):
+                H_lin_sp += self.a_H[i]*self.a[j]*Omega[i,j]
+        return H_lin_sp
+
+    def make_H(self,Omega,eps=1e-5):
+        '''
+        Make a Hamiltonian combining the linear and nonlinear parts.
+        Args:
+            Omega (complex-valued matrix) describes the Hamiltonian of the system.
+            Omega = -1j*A        #### full dynamics (not necessarily Hermitian)
+            Omega = (A-A.H)/(2j) #### closed dynamics only (Hermitian part of above)
+            eps (optional[float]): Cutoff for the significance of a particular term.
+        Returns:
+            A symbolic expression for the full Hamiltonian.
+        '''
+        H_nonlin = self.make_nonlin_H(eps)
+        H_lin = self.make_lin_H(Omega)
+        self.H = H_nonlin + H_lin
+        return self.H
+
+    def make_eq_motion(self,):
+        '''
+        Input is a tuple or list, output is a matrix vector.
+        This generates Hamilton's equations of motion for a and a^H
+
+        Returns:
+            A function that yields the Hamiltonian equations of motion based on
+            the Hamiltonian given.
+            The equations of motion take an array as an input and return a column
+            vector as an output.
+        '''
+        diff_ls = ([1j*sp.diff(self.H,var) for var in self.a_H] +
+                  [-1j*sp.diff(self.H,var) for var in self.a])
+        fs = [sp.lambdify( tuple(self.a+self.a_H),expression) for expression in diff_ls ]
+        return lambda arr: (np.asmatrix([ f(* arr ) for f in fs])).T
---
+++ Time_Sims_nonlin.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 31 2015
+
+@author: gil
+@title: Time_Sims_Nonlin.py
+"""
+
+import Roots
+import Potapov
+import Examples
+import functions
+import Hamiltonian
+
+import numpy as np
+import numpy.linalg as la
+import sympy as sp
+import itertools
+
+import matplotlib.pyplot as plt
+from scipy.integrate import ode
+
+def make_f(eq_mot,B,a_in):
+    '''
+    Equations of motion, including possibly nonlinear internal dynamics.
+    Args:
+        eq_mot: The equations of motion, which take an array and return a
+        matrix column.
+        B: The matrix multiplying the inputs to the system.
+        a_in: The inputs to the system
+
+    Returns:
+        A function that maps (t,a) -> f'(t,a), where t is a scalar (time), and
+        a is an array representing the state of the system.
+    '''
+    return lambda t,a: np.asarray(eq_mot(a)+B*a_in(t)).T[0]
+
+def make_f_lin(A,B,a_in):
+    '''
+    Linear equations of motion
+    Args:
+        A: The matrix for the linear equations of motion:
+            d(a,a^H)/dt = A(a,a^H) + B * a_in (t)
+        B: The matrix multiplying the inputs to the system.
+        a_in: The inputs to the system
+    Returns:
+        A function that maps (t,a) -> f'(t,a), where t is a scalar (time), and
+        a is an array representing the state of the system.
+    '''
+    return lambda t,a: np.asarray(A*np.asmatrix(a).T+B*a_in(t)).T[0]
+
+def run_ODE(f, a_in, C, D, num_of_variables, T = 100, dt = 0.01):
+    '''
+    Run the ODE for the given set of equations and record the outputs.
+
+    Args:
+        f (function): Evolution of the system
+        a_in (function): inputs as a function of time
+        C,D: matrices to use to obtain output from system state and input
+        num_of_variables (int): number of variables the system has
+        T (optional[positive float]): length of simulation
+        dt (optional[float]): time step used by the simulation
+    Returns:
+        An array Y of outputs
+    '''
+    r = ode(f).set_integrator('zvode', method='bdf')
+    y0 = np.asmatrix([0.]*num_of_variables).T
+    r.set_initial_value(y0, 0.)
+    Y = []
+    while r.successful() and r.t < T:
+        Y.append(C*r.y+D*a_in(r.t))
+        r.integrate(r.t+dt)
+    return Y
+
+def double_up(M1,M2=None):
+    '''
+    Takes a given matrix M1 and an optional matrix M2 and generates a
+    doubled-up matrix to use for simulations when the doubled-up notation
+    is needed. i.e.
+
+    (M1,M2) -> (M1          M2)
+               (conj(M2)    conj(M1))
+    (M1,None) -> (M1          0)
+                 (0    conj(M1))
+    Args:
+        M1: matrix to double-up
+        M2: optional second matrix to double-up
+    Returns:
+        The doubled-up matrix.
+    '''
+    if M2 == None:
+        M2 = np.zeros_like(M1)
+    top = np.hstack([M1,M2])
+    bottom = np.hstack([np.conj(M2),np.conj(M1)])
+    return np.vstack([top,bottom])
+
+if __name__ == "__main__":
+
+    Ex = Examples.Example3(r1 = 0.9, r3 = 0.9, max_linewidth=15.,max_freq=25.)
+    Ex.run_Potapov()
+    modes = functions.spatial_modes(Ex.roots,Ex.M1,Ex.E)
+
+    M = len(Ex.roots)
+
+    A,B,C,D = Potapov.get_Potapov_ABCD(Ex.roots,Ex.vecs,Ex.T,z=0.)
+    A_d,C_d,D_d = map(double_up,(A,C,D))
+    B_d = -double_up(C.H)
+
+    ham = Hamiltonian.Hamiltonian(Ex.roots,modes,Ex.delays,0,0,0.1,1000.,3,2)
+    H = ham.make_H(A_d)
+    eq_mot = ham.make_eq_motion()
+
+
+    a_in = lambda t: np.asmatrix([1e-6]*np.shape(D_d)[-1]).T  ## make a sample input function
+
+    f = make_f(eq_mot,B_d,a_in)
+    f_lin = make_f_lin(A_d,B_d,a_in)
+
+    Y = run_ODE(f_lin, a_in, C_d, D_d, 2*M, T = 100, dt = 0.01)
+
+    for i in range(2):
+        plt.plot([abs(y)[i][0,0] for y in Y ])
+        plt.savefig('sample_graph'+str(i)+'.pdf',format='pdf')
