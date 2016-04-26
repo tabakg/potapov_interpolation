89e739589424866ea4f47b1453ba782e4afe93ca
Gil Tabak <tabak.gil@gmail.com>
2016-04-25 17:12:06 -0700

    changed theme

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    #'sphinxcontrib.napoleon',
+    'sphinxcontrib.napoleon',
 ]
 
 
@@ -117,7 +117,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
