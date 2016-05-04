ab84d6ba942412425f67f25b37f86b66c3f6f7b4
Gil Tabak <tabak.gil@gmail.com>
2016-05-04 15:17:59 -0700

    changes to conf

--- Potapov_Code/__init__.py
+++ Potapov_Code/__init__.py
@@ -15,23 +15,23 @@ def contour_plot(Mat):
     cax = ax.matshow(abs(Mat), interpolation='nearest')
     fig.colorbar(cax)
 
-if __name__ == "__main__":
+if __name__ == "__main__" and False:
 
-    # Ex = Time_Delay_Network.Example3(r1 = 0.999, r3 = 0.999)
-    # Ex.run_Potapov()
-    #
-    # E = Ex.E
-    # roots = Ex.roots
-    # M1 = Ex.M1
-    # delays = Ex.delays
-    # modes = functions.spatial_modes(roots,M1,E)
-    #
-    # Mat = functions.make_normalized_inner_product_matrix(roots,modes,delays)
-    #
-    # contour_plot(Mat)
-    #
-    #
-    # print Ex.max_freq
-    # print Ex.max_linewidth
-    #
-    # print roots
+    Ex = Time_Delay_Network.Example3(r1 = 0.999, r3 = 0.999)
+    Ex.run_Potapov()
+
+    E = Ex.E
+    roots = Ex.roots
+    M1 = Ex.M1
+    delays = Ex.delays
+    modes = functions.spatial_modes(roots,M1,E)
+
+    Mat = functions.make_normalized_inner_product_matrix(roots,modes,delays)
+
+    contour_plot(Mat)
+    
+
+    print Ex.max_freq
+    print Ex.max_linewidth
+
+    print roots
